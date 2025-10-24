aff4e7f7da4a06084f4cd913966fa509ef5c68cc
Madu Biradar <madubiradar@gmail.com>
2025-10-24 14:13:12 +0530

    catalog-service set up pushed

--- catalog-service/src/main/java/com/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/catalog/CatalogServiceApplication.java
@@ -1,4 +1,4 @@
-package com.catalog.catalogservice;
+package com.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- catalog-service/src/test/java/com/catalog/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/catalog/CatalogServiceApplicationTests.java
@@ -1,8 +1,10 @@
-package com.catalog.catalogservice;
+package com.catalog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class CatalogServiceApplicationTests {
 
